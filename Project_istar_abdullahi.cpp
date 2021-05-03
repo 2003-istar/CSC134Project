a26ae379dd3dda1e4efa8a9e1dc7e1e86835a155
Istar Abdullahi <53275917+2003-istar@users.noreply.github.com>
2021-05-03 19:22:21 -0400

    Functions

--- Project_istar_abdullahi.cpp
+++ Project_istar_abdullahi.cpp
@@ -1,6 +1,6 @@
-//CSC134 Project 2
+//CSC134 Project 3
 // Name: Istar Abdullahi
-//Date: April 8, 2021
+//Date: May 1, 2021
 /* Description: This program will tell a gamer what graphics
 quality they will be able to run a newly developed video game
 on, based on the specifications of their computer's hardware.
@@ -11,6 +11,14 @@ on, based on the specifications of their computer's hardware.
 #include <iomanip>
 #include <string>
 
+//prototypes 
+void displayTitle ();
+std::string getResolutionString(int);
+double getMultiplierValue(int);
+double calculatePerformanceScore (int, int, int, double);
+std :: string getRecommendedQuality (int);
+void displayInformation (int, int, int, double, std::string, std ::string);
+
 using namespace std;
 
 int main()
@@ -19,8 +27,9 @@ int main()
 	int choice;// users choice
 	int GPUSpeed, CPUSpeed, numCores, numComp, i;// variables for the clock speed of the graphic card & processor
 	double multi, perScore= 0, highScore= 0, lowScore= 0;
+	string resolution;
+	string quality;
 	//and the number of cores
-	string recom = "\n\nComputer Hardware Graphics Quality Recommendation Tool\n\n"; // string holding the name
 	
 	do
 	{
@@ -28,7 +37,7 @@ int main()
 		cin >> numComp;
 	}while (numComp < 0);// validates the inputer number and repeates the question if invalid input is entered
 	
-	cout << recom;
+	displayTitle();
 	
 	for (i=0; i < numComp; i++)// loop through the program to calculate the amount of computers entered
 	{
@@ -69,58 +78,13 @@ int main()
 			cin >> choice;
 		}while ( choice < 0 || choice > 4);// validates the input and asks for input again if wrong.
 		
-		//Output
-		cout << "\n\nGPU Clock Speed: "<< GPUSpeed << " MHz";// print out the gpu clock speed
-		cout << "\nCPU Clock Speed: "<< CPUSpeed << " MHz";// print out the cpu clock speed
-		cout << "\nNumber of Cores: "<< numCores;//print out the number of cores
-	
-		switch(choice)// prints out the resolution that was picked
-		{
-			case 1:// if choice is 1 print out the resolution : 1280 x 720
-			{	//calculating performance score based on input
-				perScore = ((5 * GPUSpeed) + (numCores * CPUSpeed)) * 1;
-				cout << "\nMonitor Resolution: 1280 x 720";
-				break;
-			}
-			case 2:// if choice is 2 print out the resolution : 1920 x 1080
-			{	//calculating performance score based on input
-				perScore = ((5 * GPUSpeed) + (numCores * CPUSpeed)) * .75;
-				cout << "\nMonitor Resolution: 1920 x 1080";
-				break;
-			}
-			case 3:// if choice is 3 print out the resolution : 2560 x 1440
-			{	//calculating performance score based on input
-				perScore = ((5 * GPUSpeed) + (numCores * CPUSpeed)) * .55;
-				cout << "\nMonitor Resolution: 2560 x 1440";
-				break;
-			}
-			case 4:// if choice is 4 print out the resolution : 3840 x 2160
-			{	//calculating performance score based on input
-				perScore = ((5 * GPUSpeed) + (numCores * CPUSpeed)) * .35;
-				cout << "\nMonitor Resolution: 3840 x 2160";
-				break;
-			}
-		}
-		//print the performance score formatted 
-		cout << "\nPerformance Score: " << fixed << setprecision(2)<<perScore;
+		resolution = getResolutionString (choice); // stores the resolution based on the choice
+		multi = getMultiplierValue(choice); // stores the multiplier value based on the choice
 		
+		perScore = calculatePerformanceScore(GPUSpeed, CPUSpeed, numCores, multi);// calculates the performance score
+		quality = getRecommendedQuality(perScore); // holds the quality 
 		
-		if(perScore > 17000)// if more than 17,00, print ultra
-		{
-			cout << "\nRecommended Graphics Quality: Ultra\n\n";
-		}else if (perScore > 15000 )//if more than 15000, print high
-		{
-			cout << "\nRecommended Graphics Quality: High\n\n";
-		}else if (perScore > 13000 )//if more than 13,000, print medium
-		{
-			cout << "\nRecommended Graphics Quality: Medium\n\n";
-		}else if (perScore > 11000 )// if more than 11,000, print low
-		{
-			cout << "\nRecommended Graphics Quality: Low\n\n";
-		}else// if less than 11000, print unable to play
-		{
-			cout << "\nRecommended Graphics Quality: Unable to Play\n\n\n";
-		}
+		displayInformation (GPUSpeed, CPUSpeed, numCores, perScore, resolution, quality);
 		
 		if( i == 0)
 		{
@@ -143,6 +107,106 @@ int main()
 	
 	
 	return 0;
+}
+
+/*display title: 
+prints the title out whenever called. doesn't return anything. 
+*/
+void displayTitle ()
+{
+	string recom = "\n\nComputer Hardware Graphics Quality Recommendation Tool\n\n"; // string holding the name
+	cout << recom; // prints out the string variable 
+}
+
+/* getResolutionString
+A function that accepts an integer value (1, 2, 3, or 4). 
+The function should return the appropriate String representation 
+of the monitor resolution. 
+*/
+std::string getResolutionString(int choice)
+{
+	string resolution;
+	if (choice == 1)
+		resolution = "1280 x 720"; // sets the string with the resolution
+	else if(choice == 2)
+		resolution = "1920 x 1080";// sets the string with the resolution
+	else if (choice == 3)
+		resolution = "2560 x 1440";// sets the string with the resolution
+	else if (choice == 4)
+		resolution = "3840 x 2160";// sets the string with the resolution
+		
+	return resolution; // returns the type of resollution
+}
+
+/* getMultiplierValue
+A Function that accepts an integer value (1, 2, 3, or 4) that 
+denotes the monitor resolution and returns the appropriate 
+ìmultiplierî value.
+*/
+double getMultiplierValue (int choice)
+{
+	double multiValue;
+	if (choice == 1)
+		multiValue = 1.00; // sets the double with the multiplier value
+	else if(choice == 2)
+		multiValue = .75;// sets the double with the multiplier value
+	else if (choice == 3)
+		multiValue = .55;// sets the double with the multiplier value
+	else if (choice == 4)
+		multiValue = .35;// sets the double with the multiplier value
+		
+	return multiValue; // returns the mulitplier value
+}
+
+double calculatePerformanceScore (int GPUSpeed, int CPUSpeed, int cores, double multiValue)
+{
+	double perfScore;
+	perfScore = ((5 * GPUSpeed) + (cores * CPUSpeed)) * multiValue;
+	return perfScore;
+	
+}
+
+/*getRecommendedQuality
+A function that accepts a performance score as an argument and returns 
+the recommended graphics quality. 
+*/
+std :: string getRecommendedQuality (int perfScore)
+{
+	string quality; // the string variable that holds the recommended quality
+	
+	if(perfScore > 17000)// if more than 17,00, print ultra
+	{
+		quality = "Ultra";
+	}else if (perfScore > 15000 )//if more than 15000, print high
+	{
+		quality = "High";
+	}else if (perfScore > 13000 )//if more than 13,000, print medium
+	{
+		quality = "Medium";
+	}else if (perfScore > 11000 )// if more than 11,000, print low
+	{
+		quality = "Low";
+	}else// if less than 11000, print unable to play
+	{
+		quality = "Unable to Play";
+	}
+	
+	return quality; 
+}
+
+void displayInformation (int GPUSpeed, int CPUSpeed, int cores, double perScore, string resolution, string quality )
+{
+	//Output
+	cout << "\n\nGPU Clock Speed: "<< GPUSpeed << " MHz";// print out the gpu clock speed
+	cout << "\nCPU Clock Speed: "<< CPUSpeed << " MHz";// print out the cpu clock speed
+	cout << "\nNumber of Cores: "<< cores;//print out the number of cores
+	cout << "\nMonitor Resolution: " << resolution; // prints out the type of resolution
+
+	//print the performance score formatted 
+	cout << "\nPerformance Score: " << fixed << setprecision(2)<<perScore;
+	cout << "\nRecommended Graphics Quality: " << quality << "\n";
+	
+	
 }
 
 
